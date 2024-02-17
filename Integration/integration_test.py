3e2cab12db62c1028e22326d7b638d3c68703022
manjot4308 <singh1191manjot@gmail.com>
2024-02-16 21:01:38 -0500

    Added 2 new files in Integration folder.

--- Integration/integration_test.py
+++ Integration/integration_test.py
@@ -1,1 +1,12 @@
+from sorting import sort_five_numbers
+
+# Positive test case (should pass)
+def test_sort_five_numbers_positive():
+    sorted_nums = sort_five_numbers(5, 3, 9, 1, 7)
+    assert sorted_nums == [1, 3, 5, 7, 9], "Sorting failed for positive case"
+
+# Negative test case (should fail)
+def test_sort_five_numbers_negative():
+    sorted_nums = sort_five_numbers(5, 3, 9, 1, 7)
+    assert sorted_nums == [5, 3, 9, 1, 7], "Sorting failed for negative case"
 
--- Integration/sorting.py
+++ Integration/sorting.py
@@ -1,1 +1,6 @@
+def sort_five_numbers(num1, num2, num3, num4, num5):
+    numbers = [num1, num2, num3, num4, num5]
 
+    sorted_numbers = sorted(numbers) #sorting
+
+    return sorted_numbers
